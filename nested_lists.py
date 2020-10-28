ef5057bb13c1728578dd6eec65c6f717d9daba44
dylanPMurphy <dylanmurphydba@gmail.com>
2020-10-28 00:23:12 -0700

    nested list

---
+++ nested_lists.py
@@ -0,0 +1,9 @@
+python_students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]] 
+
+def sort_2(arr):
+    min_val  = 10000.0
+    second_min = 0
+    names = []
+    for i in arr:
+        if arr[i,1] < min :
+            
